a6c1b56c7ee64cf5526a86972db47f3177f842eb
Alberto Luiz Pontes Siqueira <34726832+albertolpsiqueira@users.noreply.github.com>
2025-01-23 16:23:44 -0300

    Homolog OK

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImageUrl();
     String getShortDescription();
     Integer getPosition();
